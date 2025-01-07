91d8b75d1b45c812c21d3c2608f344582e333764
TranQuan0406 <154067599+tranquan04062009@users.noreply.github.com>
2025-01-07 18:13:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 os.system("pip install scikit-learn")
-os.system("pip install tensorflow==2.11.0")
+os.system("pip install tensorflow")
+os.system("pip install tensorflow-cpu")
 import random
 from collections import Counter, deque
 from sklearn.naive_bayes import GaussianNB
