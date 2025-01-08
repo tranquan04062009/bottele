ca061a1886e679fb9040b0f25a11391c1beeb20d
TranQuan0406 <154067599+tranquan04062009@users.noreply.github.com>
2025-01-08 08:20:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ model = LogisticRegression()
 
 last_prediction = None
 
-model = RandomForestClassifier(n_estimators=100, random_state=42
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 
 def train_model():
     """
